5bdb7c4af926c3a8295183df7537549389eb6aec
TheHypnoo <mysterlegendz@gmail.com>
2021-01-14 20:17:33 +0100

    A falta de corregir el Spinner de la categoria del producto! Falta eso, filtrar y editar.

--- app/src/main/java/com/sergigonzalez/gestionarticulos/newArticle.kt
+++ app/src/main/java/com/sergigonzalez/gestionarticulos/newArticle.kt
@@ -5,8 +5,7 @@ import android.database.sqlite.SQLiteConstraintException
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.sergigonzalez.gestionarticulos.data.Article
@@ -20,8 +19,10 @@ class newArticle : AppCompatActivity() {
     private lateinit var nombre_et: EditText
     private lateinit var precio_et: EditText
     private lateinit var descripcion_et: EditText
+    private lateinit var familySpinner: Spinner
     private lateinit var stock_et: EditText
     private lateinit var save_btn: Button
+    private var family: String = " "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +32,12 @@ class newArticle : AppCompatActivity() {
 
         nombre_et = findViewById(R.id.edtCode)
         descripcion_et = findViewById(R.id.edtDes)
+        familySpinner = findViewById(R.id.family_spinner)
         precio_et = findViewById(R.id.edtPVP)
         stock_et = findViewById(R.id.edtStock)
         save_btn = findViewById(R.id.save_btn)
+
+        val database = ArticleApp.getDatabase(this)
 
         if (intent.hasExtra("Article")) {
             val article = intent.extras?.getSerializable("Article") as Article
@@ -45,7 +49,30 @@ class newArticle : AppCompatActivity() {
             idArticle = article.idArticle
         }
 
-        val database = ArticleApp.getDatabase(this)
+
+
+        ArrayAdapter.createFromResource(this, R.array.family, android.R.layout.simple_spinner_item).also {
+            adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            familySpinner.adapter = adapter
+        }
+
+        familySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //Null
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                if (selectedItem.equals("Hardware")) {
+                    family = "Hardware"
+                }else if(selectedItem.equals("Software")) {
+                    family = "Software"
+                } else if(selectedItem.equals("Altres")){
+                    family = "Altres"
+                }
+            }
+        }
 
         save_btn.setOnClickListener {
             val id : String
@@ -73,6 +100,15 @@ class newArticle : AppCompatActivity() {
                     imm.hideSoftInputFromWindow(view.windowToken, 0)
                 }
                 snackbarMessage("Debes introducir una descripción")
+                return@setOnClickListener
+            }
+
+            if(family.equals(" ")) {
+                if (view != null) {
+                    val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(view.windowToken, 0)
+                }
+                snackbarMessage("Debes introducir una categoria")
                 return@setOnClickListener
             }
 
@@ -116,7 +152,7 @@ class newArticle : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val article = Article(id, descripcion, "", precio, stock)
+            val article = Article(id, descripcion, family, precio, stock)
 
                 if (idArticle != null) {
 
