271f8c3925c758851c73d6d7ae91b4b83aec0e2c
TheHypnoo <mysterlegendz@gmail.com>
2021-01-29 11:37:58 +0100

    Funcionamiento de los Movimientos al ver todos los articulos realizado! Terminado 100%

--- app/src/main/java/com/sergigonzalez/gestionarticulos/Movements_All.kt
+++ app/src/main/java/com/sergigonzalez/gestionarticulos/Movements_All.kt
@@ -1,40 +1,123 @@
 package com.sergigonzalez.gestionarticulos
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
+import android.view.*
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.sergigonzalez.gestionarticulos.data.ArticleApp
+import com.sergigonzalez.gestionarticulos.data.Movement
+import java.text.ParseException
 
 class Movements_All : AppCompatActivity() {
+    private var _list: ListView? = null
     private lateinit var edt : EditText
     private lateinit var _calendar : ImageView
+    private var listMovements: List<Movement> = emptyList()
+    val database = ArticleApp.getDatabase(this)
+    private val adapter: Movements_all_Adapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movements_all)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         edt = findViewById(R.id.edtDateM)
+
+        _list = findViewById(R.id.list_movement_all)
+
         _calendar = findViewById(R.id.calendarM)
         _calendar.setOnClickListener{
             DialogCalendar.Dialog(this@Movements_All, edt)
         }
 
+        val btn = findViewById<View>(R.id.btnSearchM) as FloatingActionButton
+        btn.setOnClickListener {
+            try {
+                SearchMovements()
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
+        }
 
         val btnR = findViewById<View>(R.id.btnResetM) as FloatingActionButton
         btnR.setOnClickListener { resetMovements() }
-
 
     }
 
     private fun resetMovements(){
         edt.setText("")
+        _list?.visibility = View.GONE
     }
+
+    fun SearchMovements(){
+        if (!edt.text.toString().isEmpty()) {
+            val date = DialogCalendar.ChangeFormatDate(edt.text.toString(), "dd/MM/yyyy", "yyyy/MM/dd")
+            database.Articles().dateMovements(date).observe(this, Observer {
+                listMovements = it
+                val adapter = Movements_all_Adapter(this, listMovements)
+
+                _list!!.adapter = adapter
+
+            })
+            _list?.visibility = View.VISIBLE
+        } else {
+            database.Articles().dateMovementsAll().observe(this, Observer {
+                listMovements = it
+                val adapter = Movements_all_Adapter(this, listMovements)
+
+                _list!!.adapter = adapter
+
+            })
+            _list?.visibility = View.VISIBLE
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+}
+
+class Movements_all_Adapter(private val mContext: Context, private val listMovements: List<Movement>) : ArrayAdapter<Movement>(
+        mContext,
+        0,
+        listMovements
+) {
+
+    private lateinit var database: ArticleApp
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layout = LayoutInflater.from(mContext).inflate(
+                R.layout.elements_movements_all,
+                parent,
+                false
+        )
+        database = ArticleApp.getDatabase(mContext)
+        val movement = listMovements[position]
+
+        val code = layout.findViewById<TextView>(R.id.tvCodeM)
+
+        val day = layout.findViewById<TextView>(R.id.tvDateM)
+
+        val quantity = layout.findViewById<TextView>(R.id.tvCanM)
+
+        val type = layout.findViewById<TextView>(R.id.tvTypeM)
+
+        code.text = movement.idArticleMovement
+
+        quantity.text = movement.quantity.toString()
+
+        type.text = movement.type.toString()
+
+        try {
+            day.text = DialogCalendar.ChangeFormatDate(movement.day, "yyyy/MM/dd", "dd/MM/yyyy")
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+
+        return layout
     }
 }
--- app/src/main/java/com/sergigonzalez/gestionarticulos/data/ArticleDAO.kt
+++ app/src/main/java/com/sergigonzalez/gestionarticulos/data/ArticleDAO.kt
@@ -5,11 +5,16 @@ import androidx.room.*
 
 @Dao
 interface ArticleDAO {
+
+    //getAll
+
     @Query("SELECT * FROM Articles")
     fun getAll(): LiveData<List<Article>>
 
     @Query("SELECT * FROM MOVEMENT")
     fun getAllMovements(): LiveData<List<Movement>>
+
+    //Filtros para el Articulo
 
     @Query("SELECT * FROM Articles ORDER BY idArticle ASC")
     fun getAllAsc(): LiveData<List<Article>>
@@ -29,6 +34,8 @@ interface ArticleDAO {
     @Query("SELECT * FROM Articles WHERE stockArticle <= 0")
     fun getWithoutStock(): LiveData<List<Article>>
 
+    //Movimientos del articulo
+
     @Query("SELECT * FROM Movement WHERE day <= :dateF AND idArticleMovement = :idArticle ORDER BY day DESC")
     fun dateF(idArticle: String, dateF: String): LiveData<List<Movement>>
 
@@ -38,6 +45,15 @@ interface ArticleDAO {
     @Query("SELECT * FROM Movement WHERE day BETWEEN :dateI AND :dateF AND idArticleMovement = :idArticle ORDER BY day DESC")
     fun dateIDateF(idArticle: String, dateF: String, dateI: String): LiveData<List<Movement>>
 
+    //Movimientos de todos los articulos
+    @Query("SELECT * FROM  Articles INNER JOIN Movement ON idArticle = idArticleMovement WHERE day =:date ORDER BY idArticle ASC")
+    fun dateMovements(date: String): LiveData<List<Movement>>
+
+    @Query("SELECT * FROM Articles INNER JOIN Movement ON idArticle = idArticleMovement ORDER BY idArticle ASC")
+    fun dateMovementsAll(): LiveData<List<Movement>>
+
+
+    //Extras
 
     @Insert
     fun insertAll(vararg Articles: Article)
